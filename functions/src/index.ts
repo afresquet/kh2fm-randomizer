c0acca43053eaf7212773f8d92fad1936824657d
afresquet <alvarofresquet@gmail.com>
2020-05-10 19:12:50 +0200

    Seed JWT generation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,22 +1,38 @@
+import express from "express";
 import * as functions from "firebase-functions";
+import { sign, verify } from "jsonwebtoken";
 import { Configuration } from "./Configuration";
 import { createPnach } from "./logic/createPnach";
 
-export const randomizer = functions.https.onRequest(async (req, res) => {
-	// would get this from the request
-	const configuration: Configuration = {
-		seed: req.query.seed as string,
-		stats: true,
-		criticalMode: true,
-		donaldAbilities: true,
-		goofyAbilities: true,
-	};
+const { secret, algorithm } = functions.config().randomizer;
 
-	const pnach = createPnach({
-		...configuration,
-		seed: configuration.seed || Date.now().toString(),
-	});
+const app = express();
 
-	res.set({ "Content-Disposition": 'attachment; filename="F266B00B.pnach"' });
-	res.send(pnach);
+app.use(express.json());
+
+app.post("/seed", (req, res) => {
+	const seed = sign(
+		{ ...req.body, seed: req.body.seed || Date.now().toString() },
+		secret,
+		{ algorithm }
+	);
+
+	res.json({ seed });
 });
+
+app.get("/seed/:seed", (req, res) => {
+	try {
+		const configuration = verify(req.params.seed, secret, {
+			algorithms: [algorithm],
+		});
+
+		const pnach = createPnach(configuration as Configuration);
+
+		res.set({ "Content-Disposition": 'attachment; filename="F266B00B.pnach"' });
+		res.send(pnach);
+	} catch (error) {
+		res.status(400).json({ error });
+	}
+});
+
+export const randomizer = functions.https.onRequest(app);
