1687df9ce18eb56bd8732a5667c9c3daa03e62b7
afresquet <alvarofresquet@gmail.com>
2020-05-11 01:01:08 +0200

    Maybe this fixes gh pages?

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>,
