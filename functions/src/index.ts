20553d0670807eb7716a24965756d5ee9bd02792
afresquet <alvarofresquet@gmail.com>
2020-05-10 16:07:39 +0200

    Primitive randomization of rewards

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,24 @@ import * as functions from "firebase-functions";
 import { createLine } from "./helpers/createLine";
 import { levels } from "./levels";
 import { LevelStats } from "./LevelStats";
+import { rewardLocations } from "./rewardLocations";
+import { rewards as originalRewards } from "./rewards";
 
 export const randomizer = functions.https.onRequest(async (_, res) => {
 	const content = [];
+
+	const rewards = originalRewards.slice();
+	const randomizedLocations = rewardLocations
+		.map(location => {
+			const randomIndex = Math.floor(Math.random() * rewards.length);
+			const reward = rewards.splice(randomIndex, 1)[0];
+
+			return { location, reward };
+		})
+		.reduce((result, random) => {
+			return result + createLine(random.location.value, random.reward.value);
+		}, "");
+	content.push(randomizedLocations);
 
 	const levelStats = new LevelStats();
 	const randomizedStats = levels.reduce((result, level) => {
