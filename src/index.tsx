d3107ac8aed40af5b18dacc8b0b4de56b58e5f99
afresquet <alvarofresquet@gmail.com>
2020-05-11 03:57:39 +0200

    Hash router

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<HashRouter basename={process.env.PUBLIC_URL}>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
