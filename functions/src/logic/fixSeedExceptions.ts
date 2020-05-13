e798f02639360217034e16438418b0d1432f87f4
afresquet <alvarofresquet@gmail.com>
2020-05-13 02:43:37 +0200

    Fixed ability on popup bug

--- functions/src/logic/fixSeedExceptions.ts
+++ functions/src/logic/fixSeedExceptions.ts
@@ -1,6 +1,7 @@
 import seedrandom from "seedrandom";
 import { Configuration } from "../Configuration";
-import { Reward } from "../rewards/Reward";
+import { RewardLocationType } from "../rewardLocations/RewardLocation";
+import { Reward, RewardType } from "../rewards/Reward";
 import { Seed } from "./createSeed";
 
 const getReplaceable = (
@@ -37,6 +38,36 @@ const getReplaceable = (
 	);
 };
 
+const getReplaceableByType = (
+	seed: Seed[],
+	typeToOmit: RewardLocationType,
+	configuration: Configuration,
+	nextIndex?: number
+): Seed => {
+	let index = nextIndex;
+
+	if (!nextIndex) {
+		const seeder = seedrandom(
+			configuration.seed + "replaceable-type" + seed.length.toString()
+		);
+
+		index = Math.floor(seeder() * seed.length);
+	}
+
+	const candidate = seed[index!];
+
+	if (candidate.location.type !== typeToOmit) {
+		return candidate;
+	}
+
+	return getReplaceableByType(
+		seed,
+		typeToOmit,
+		configuration,
+		(index! + 1) % seed.length
+	);
+};
+
 export const fixSeedExceptions = (
 	seed: Seed[],
 	configuration: Configuration
@@ -48,6 +79,24 @@ export const fixSeedExceptions = (
 			]?.exclude?.includes(iteration.reward)
 		) {
 			const replaceable = getReplaceable(seed, iteration.reward, configuration);
+
+			[iteration.reward, replaceable.reward] = [
+				replaceable.reward,
+				iteration.reward,
+			];
+		}
+	}
+
+	for (const iteration of seed) {
+		if (
+			iteration.location.type === RewardLocationType.POPUP &&
+			iteration.reward.type === RewardType.ABILITY
+		) {
+			const replaceable = getReplaceableByType(
+				seed,
+				RewardLocationType.POPUP,
+				configuration
+			);
 
 			[iteration.reward, replaceable.reward] = [
 				replaceable.reward,
