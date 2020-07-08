83ed383827816f9c84f5a9bf32d793888825b238
afresquet <alvarofresquet@gmail.com>
2020-07-08 17:09:27 +0200

    Port Royal enemy locations

---
+++ src/enemyLocations/portRoyal.ts
@@ -0,0 +1,478 @@
+import { Enemies } from "../enemies";
+import { LocationName } from "../LocationName";
+import { EnemyLocation, EnemyLocationType } from "./EnemyLocation";
+
+export const portRoyalEnemyLocations: EnemyLocation[] = [
+	// {
+	// 	type: EnemyLocationType.MOB_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Harbor Pirates (1st Visit; Part I)",
+	// 	value: "11C42BB0 ",
+	// 	enemy: Enemies.???,
+	// 	// | Undead Pirate B |
+	// },
+	// {
+	// 	type: EnemyLocationType.MOB_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Harbor Pirates (1st Visit; Part I)",
+	// 	value: "11C42C30",
+	// 	enemy: Enemies.???,
+	// 	// | Undead Pirate C |
+	// },
+	// {
+	// 	type: EnemyLocationType.MOB_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Harbor Pirates (1st Visit; Part I)",
+	// 	value: "11C42C70",
+	// 	enemy: Enemies.???,
+	// 	// | Undead Pirate C |
+	// },
+	// {
+	// 	type: EnemyLocationType.MOB_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Harbor Pirates (1st Visit; Part I)",
+	// 	value: "11C42CB0",
+	// 	enemy: Enemies.???,
+	// 	// | Undead Pirate C |
+	// },
+	// {
+	// 	type: EnemyLocationType.MOB_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Harbor Pirates (1st Visit; Part I)",
+	// 	value: "11C42DE4",
+	// 	enemy: Enemies.???,
+	// 	// | Undead Pirate C |
+	// },
+	// {
+	// 	type: EnemyLocationType.MOB_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Harbor Pirates (1st Visit; Part I)",
+	// 	value: "11C42E24",
+	// 	enemy: Enemies.???,
+	// 	// | Undead Pirate B |
+	// },
+	// {
+	// 	type: EnemyLocationType.MOB_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Harbor Pirates (1st Visit; Part I)",
+	// 	value: "11C42E64",
+	// 	enemy: Enemies.???,
+	// 	// | Undead Pirate C |
+	// },
+	// {
+	// 	type: EnemyLocationType.MOB_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Harbor Pirates (1st Visit; Part I)",
+	// 	value: "11C42EA4",
+	// 	enemy: Enemies.???,
+	// 	// | Undead Pirate C |
+	// },
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Town Heartless",
+		value: "11C56984",
+		enemy: Enemies.SHADOW,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Town Heartless",
+		value: "11C569C4",
+		enemy: Enemies.SHADOW,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Town Heartless",
+		value: "11C56A04",
+		enemy: Enemies.SHADOW,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Town Heartless",
+		value: "11C56A44",
+		enemy: Enemies.SHADOW,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Town Heartless",
+		value: "11C56A84",
+		enemy: Enemies.SHADOW,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Town Heartless",
+		value: "11C56AC4",
+		enemy: Enemies.SHADOW,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Town Heartless",
+		value: "11C56BA0",
+		enemy: Enemies.SHADOW,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Town Heartless",
+		value: "11C56BE0",
+		enemy: Enemies.SHADOW,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Town Heartless",
+		value: "11C56C20",
+		enemy: Enemies.SHADOW,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Town Heartless",
+		value: "11C56C60",
+		enemy: Enemies.CANNON_GUN,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Town Heartless",
+		value: "11C56CA0",
+		enemy: Enemies.CANNON_GUN,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Town Heartless",
+		value: "11C56CE0",
+		enemy: Enemies.CANNON_GUN,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Town Heartless",
+		value: "11C56D20",
+		enemy: Enemies.SOLDIER,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Town Heartless",
+		value: "11C56D60",
+		enemy: Enemies.SOLDIER,
+	},
+	// Isla de Muerta: Cave Mouth Pirates (1 Minute Fight)
+	// 11C58104 0000???? - Undead Pirate A (Left)
+	// 11C58084 0000???? - Undead Pirate C (Middle)
+	// 11C580C4 0000???? - Undead Pirate A (Right)
+	// 11C58170 0000????
+	// 11C581B0 0000????
+	// 11C581F0 0000????
+	// 11C58230 0000????
+	// 11C58270 0000????
+	// {
+	// 		type: EnemyLocationType.MOB_FIGHT,
+	// 		location: LocationName.PORT_ROYAL,
+	// 		description: "The Interceptor Pirates Fight",
+	// 		value: "11C4E1E0",
+	// 		enemy: Enemies.???,
+	// 		// | Undead Pirate A |
+	// 	},
+	// 	{
+	// 		type: EnemyLocationType.MOB_FIGHT,
+	// 		location: LocationName.PORT_ROYAL,
+	// 		description: "The Interceptor Pirates Fight",
+	// 		value: "11C4E220",
+	// 		enemy: Enemies.???,
+	// 		// | Undead Pirate C |
+	// 	},
+	// 	{
+	// 		type: EnemyLocationType.MOB_FIGHT,
+	// 		location: LocationName.PORT_ROYAL,
+	// 		description: "The Interceptor Pirates Fight",
+	// 		value: "11C4E260",
+	// 		enemy: Enemies.???,
+	// 		// | Undead Pirate A |
+	// 	},
+	// 	{
+	// 		type: EnemyLocationType.MOB_FIGHT,
+	// 		location: LocationName.PORT_ROYAL,
+	// 		description: "The Interceptor Pirates Fight",
+	// 		value: "11C4E2A0",
+	// 		enemy: Enemies.???,
+	// 		// | Undead Pirate B |
+	// 	},
+	// 	{
+	// 		type: EnemyLocationType.MOB_FIGHT,
+	// 		location: LocationName.PORT_ROYAL,
+	// 		description: "The Interceptor Pirates Fight",
+	// 		value: "11C4E2E0",
+	// 		enemy: Enemies.???,
+	// 		// | Undead Pirate A |
+	// 	},
+	// 	{
+	// 		type: EnemyLocationType.MOB_FIGHT,
+	// 		location: LocationName.PORT_ROYAL,
+	// 		description: "The Interceptor Pirates Fight",
+	// 		value: "11C4E320",
+	// 		enemy: Enemies.???,
+	// 		// | Undead Pirate A |
+	// 	},
+	// 	{
+	// 		type: EnemyLocationType.MOB_FIGHT,
+	// 		location: LocationName.PORT_ROYAL,
+	// 		description: "The Interceptor Pirates Fight",
+	// 		value: "11C4E360",
+	// 		enemy: Enemies.???,
+	// 		// | Undead Pirate B |
+	// 	},
+	// 	{
+	// 		type: EnemyLocationType.MOB_FIGHT,
+	// 		location: LocationName.PORT_ROYAL,
+	// 		description: "The Interceptor Pirates Fight",
+	// 		value: "11C4E3A0",
+	// 		enemy: Enemies.???,
+	// 		// | Undead Pirate A |
+	// 	},
+	// 	{
+	// 		type: EnemyLocationType.MOB_FIGHT,
+	// 		location: LocationName.PORT_ROYAL,
+	// 		description: "The Interceptor Pirates Fight",
+	// 		value: "11C4E3E0",
+	// 		enemy: Enemies.???,
+	// 		// | Undead Pirate A |
+	// 	},
+	// 	{
+	// 		type: EnemyLocationType.MOB_FIGHT,
+	// 		location: LocationName.PORT_ROYAL,
+	// 		description: "The Interceptor Pirates Fight",
+	// 		value: "11C4E420",
+	// 		enemy: Enemies.???,
+	// 		// | Undead Pirate B |
+	// 	},
+	// 	{
+	// 		type: EnemyLocationType.MOB_FIGHT,
+	// 		location: LocationName.PORT_ROYAL,
+	// 		description: "The Interceptor Pirates Fight",
+	// 		value: "11C4E460",
+	// 		enemy: Enemies.???,
+	// 		// | Undead Pirate A |
+	// 	},
+	// 	{
+	// 		type: EnemyLocationType.MOB_FIGHT,
+	// 		location: LocationName.PORT_ROYAL,
+	// 		description: "The Interceptor Pirates Fight",
+	// 		value: "11C4E4A0",
+	// 		enemy: Enemies.???,
+	// 		// | Undead Pirate B |
+	// 	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "The Interceptor Barrels",
+		value: "11C52604",
+		enemy: Enemies.AIR_PIRATE,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "The Interceptor Barrels",
+		value: "11C526E0",
+		enemy: Enemies.AIR_PIRATE,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "The Interceptor Barrels",
+		value: "11C52720",
+		enemy: Enemies.AIR_PIRATE,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "The Interceptor Barrels",
+		value: "11C52760",
+		enemy: Enemies.AIR_PIRATE,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "The Interceptor Barrels",
+		value: "11C5283C",
+		enemy: Enemies.CANNON_GUN,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "The Interceptor Barrels",
+		value: "11C52918",
+		enemy: Enemies.CANNON_GUN,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "The Interceptor Barrels",
+		value: "11C529F4",
+		enemy: Enemies.CANNON_GUN,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "The Interceptor Barrels",
+		value: "11C52AD0",
+		enemy: Enemies.CANNON_GUN,
+	},
+	// {
+	// 	type: EnemyLocationType.BOSS_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Barbossa Boss Fight",
+	// 	value: "11C43370",
+	// 	enemy: Enemies.???,
+	// 	// | Illuminator |
+	// },
+	// {
+	// 	type: EnemyLocationType.BOSS_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Barbossa Boss Fight",
+	// 	value: "11C43330",
+	// 	enemy: Enemies.???,
+	// },
+	// {
+	// 	type: EnemyLocationType.MOB_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Harbor Pirates Fight (2nd Visit)",
+	// 	value: "11C42FD8",
+	// 	enemy: Enemies.???,
+	// 	// | Undead Pirate B |
+	// },
+	// {
+	// 	type: EnemyLocationType.MOB_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Harbor Pirates Fight (2nd Visit)",
+	// 	value: "11C43018",
+	// 	enemy: Enemies.???,
+	// 	// | Undead Pirate A |
+	// },
+	// {
+	// 	type: EnemyLocationType.MOB_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Harbor Pirates Fight (2nd Visit)",
+	// 	value: "11C43058",
+	// 	enemy: Enemies.???,
+	// 	// | Undead Pirate A |
+	// },
+	{
+		type: EnemyLocationType.BOSS_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Grim Reaper I Boss Fight",
+		value: "11C5BA78",
+		enemy: Enemies.GRIM_REAPER,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Gambler (Ship Graveyard: Seadrift Keep; 1st Fight)",
+		value: "11C56F38",
+		enemy: Enemies.GAMBLER,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Gambler (Ship Graveyard: Seadrift Keep; 1st Fight)",
+		value: "11C5706C",
+		enemy: Enemies.GAMBLER,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Gambler (From Ship Graveyard: The Interceptor's Hold)",
+		value: "11C5B970",
+		enemy: Enemies.GAMBLER,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Gambler (Ship Graveyard: Seadrift Row)",
+		value: "11C5B9B0",
+		enemy: Enemies.GAMBLER,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Gambler (Isla de Muerta: Cave Mouth)",
+		value: "11C58324",
+		enemy: Enemies.GAMBLER,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Gambler (Isla de Muerta: Powder Store)",
+		value: "11C5BC28",
+		enemy: Enemies.GAMBLER,
+	},
+	// {
+	// 	type: EnemyLocationType.MOB_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Gambler (Isla de Muerta: Moonlight Nook)",
+	// 	value: "11C5BC38",
+	// 	enemy: Enemies.???,
+	// 	// | Undead Pirate C |
+	// },
+	// {
+	// 	type: EnemyLocationType.MOB_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Gambler (Isla de Muerta: Moonlight Nook)",
+	// 	value: "11C5BC78",
+	// 	enemy: Enemies.???,
+	// 	// | Undead Pirate C |
+	// },
+	// {
+	// 	type: EnemyLocationType.MOB_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Gambler (Isla de Muerta: Moonlight Nook)",
+	// 	value: "11C5BCB8",
+	// 	enemy: Enemies.???,
+	// 	// | Undead Pirate C |
+	// },
+	// {
+	// 	type: EnemyLocationType.MOB_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Gambler (Isla de Muerta: Moonlight Nook)",
+	// 	value: "11C5BCF8",
+	// 	enemy: Enemies.???,
+	// 	// | Undead Pirate C |
+	// },
+	// {
+	// 	type: EnemyLocationType.MOB_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Gambler (Isla de Muerta: Moonlight Nook)",
+	// 	value: "11C5BD38",
+	// 	enemy: Enemies.???,
+	// 	// | Undead Pirate C |
+	// },
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Gambler (Isla de Muerta: Moonlight Nook)",
+		value: "11C5BD78",
+		enemy: Enemies.GAMBLER,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PORT_ROYAL,
+		description: "Gambler (Isla de Muerta: Treasure Heap)",
+		value: "11C433E4",
+		enemy: Enemies.GAMBLER,
+	},
+	// {
+	// 	type: EnemyLocationType.BOSS_FIGHT,
+	// 	location: LocationName.PORT_ROYAL,
+	// 	description: "Grim Reaper II Boss Fight",
+	// 	value: "11C4318C",
+	// 	enemy: Enemies.GRIM_REAPER,
+	// },
+];
