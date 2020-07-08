8b0e1d6ed1e5ed7fbbee3d44895f9c1655b67b49
afresquet <alvarofresquet@gmail.com>
2020-07-08 18:00:11 +0200

    Pride Lands enemy locations

---
+++ src/enemyLocations/prideLands.ts
@@ -0,0 +1,84 @@
+import { Enemies } from "../enemies";
+import { LocationName } from "../LocationName";
+import { EnemyLocation, EnemyLocationType } from "./EnemyLocation";
+
+export const prideLandsLocations: EnemyLocation[] = [
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PRIDE_LANDS,
+		description: "Elephant Graveyard Heartless",
+		value: "11C4EC14",
+		enemy: Enemies.LIVING_BONE,
+	},
+	{
+		type: EnemyLocationType.MOB_FIGHT,
+		location: LocationName.PRIDE_LANDS,
+		description: "Elephant Graveyard Heartless",
+		value: "11C4EC54",
+		enemy: Enemies.LIVING_BONE,
+	},
+	// {
+	// 	type: EnemyLocationType.BOSS_FIGHT,
+	// 	location: LocationName.PRIDE_LANDS,
+	// 	description: "Hyenas Fight I",
+	// 	value: "11C54C84",
+	// 	enemy: Enemies.???,
+	// 	// | Pumba |
+	// },
+	{
+		type: EnemyLocationType.BOSS_FIGHT,
+		location: LocationName.PRIDE_LANDS,
+		description: "Hyenas Fight I",
+		value: "11C54C44",
+		enemy: Enemies.SHENZI,
+	},
+	{
+		type: EnemyLocationType.BOSS_FIGHT,
+		location: LocationName.PRIDE_LANDS,
+		description: "Hyenas Fight I",
+		value: "11C54C04",
+		enemy: Enemies.BANZAI,
+	},
+	{
+		type: EnemyLocationType.BOSS_FIGHT,
+		location: LocationName.PRIDE_LANDS,
+		description: "Hyenas Fight I",
+		value: "11C54BC4",
+		enemy: Enemies.ED,
+	},
+	{
+		type: EnemyLocationType.BOSS_FIGHT,
+		location: LocationName.PRIDE_LANDS,
+		description: "Scar Boss Fight",
+		value: "11C5A74C",
+		enemy: Enemies.SCAR,
+	},
+	{
+		type: EnemyLocationType.BOSS_FIGHT,
+		location: LocationName.PRIDE_LANDS,
+		description: "Hyenas II",
+		value: "11C4EDB4",
+		enemy: Enemies.SHENZI,
+	},
+	{
+		type: EnemyLocationType.BOSS_FIGHT,
+		location: LocationName.PRIDE_LANDS,
+		description: "Hyenas II",
+		value: "11C4EE34",
+		enemy: Enemies.BANZAI,
+	},
+	{
+		type: EnemyLocationType.BOSS_FIGHT,
+		location: LocationName.PRIDE_LANDS,
+		description: "Hyenas II",
+		value: "11C4EDF4",
+		enemy: Enemies.ED,
+	},
+	// {
+	// 	type: EnemyLocationType.BOSS_FIGHT,
+	// 	location: LocationName.PRIDE_LANDS,
+	// 	description: "Groundshaker Boss Fight",
+	// 	value: "11C59F80",
+	// 	enemy: Enemies.???,
+	// },
+];
