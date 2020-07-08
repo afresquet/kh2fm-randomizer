b8550728ff47409a3b92ab37b7dfeb7d08d79dd4
afresquet <alvarofresquet@gmail.com>
2020-07-08 15:07:07 +0200

    Enemy digits

---
+++ src/enemies/miscellaneous.ts
@@ -0,0 +1,44 @@
+import { EnemyType } from "./Enemy";
+
+export const miscellaneous = {
+	DEMYX_WATER_CLONE: {
+		type: EnemyType.MISCELLANEOUS,
+		name: "Demyx's Water Clone",
+		value: "04A0",
+	},
+	LUXORD_CARD: {
+		type: EnemyType.MISCELLANEOUS,
+		name: "Luxord's Card",
+		value: "05FF",
+	},
+	BEES_100AC: {
+		type: EnemyType.MISCELLANEOUS,
+		name: "Bees (100 Acre Wood)",
+		value: "0625",
+	},
+	SCAR_GHOST: {
+		type: EnemyType.MISCELLANEOUS,
+		name: "Scar’s Ghost",
+		value: "0833",
+	},
+	BEES_TT: {
+		type: EnemyType.MISCELLANEOUS,
+		name: "Bees (Twilight Town)",
+		value: "085A",
+	},
+	BEES_BUMBLE_BUSTER: {
+		type: EnemyType.MISCELLANEOUS,
+		name: "Bees (Bumble Buster)",
+		value: "085B",
+	},
+	VIVI_CLONE: {
+		type: EnemyType.MISCELLANEOUS,
+		name: "Vivi Clone",
+		value: "0208D0",
+	},
+	ANTI_SORA: {
+		type: EnemyType.MISCELLANEOUS,
+		name: "Anti-Sora",
+		value: "0934",
+	},
+};
