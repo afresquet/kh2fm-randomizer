9181f5bd27337d7457611246d85d3589262772fb
afresquet <alvarofresquet@gmail.com>
2020-07-16 20:33:56 +0200

    Go Mode description

--- src/components/Seed/SettingSlider.tsx
+++ src/components/Seed/SettingSlider.tsx
@@ -12,6 +12,7 @@ export interface SettingSliderProps extends SliderProps {
 }
 
 export const SettingSlider: React.FC<SettingSliderProps> = ({
+	children,
 	title,
 	marks = Marks.onOff,
 	...props
@@ -25,6 +26,8 @@ export const SettingSlider: React.FC<SettingSliderProps> = ({
 			<div style={{ padding: "0 32px" }}>
 				<Slider max={max} marks={marks} tooltipVisible={false} {...props} />
 			</div>
+
+			{children}
 		</div>
 	);
 };
--- src/components/Seed/TabPaneGameModeSettings.tsx
+++ src/components/Seed/TabPaneGameModeSettings.tsx
@@ -3,11 +3,13 @@ import { SliderValue } from "antd/lib/slider";
 import React, { useContext } from "react";
 import { SeedContext } from "../../context/seed";
 import { useValueMapper } from "../../hooks/useValueMapper";
+import { Leveling, Toggle } from "../../settings/enums";
 import { GoAModSettings } from "../../settings/GoAModSettings";
 import { SettingSlider } from "./SettingSlider";
 
 export const TabPaneGameModeSettings: React.FC = () => {
 	const {
+		configuration,
 		gameMode: { goa },
 	} = useContext(SeedContext);
 
@@ -18,7 +20,16 @@ export const TabPaneGameModeSettings: React.FC = () => {
 			<div className="tab-pane">
 				<SettingSlider title="Promise Charm" {...mapValue("promiseCharm")} />
 
-				<SettingSlider title="Go Mode" {...mapValue("goMode")} />
+				<SettingSlider
+					title="Go Mode"
+					{...mapValue("goMode")}
+					disabled={
+						configuration.settings.criticalMode === Toggle.OFF &&
+						configuration.settings.leveling === Leveling.LEVEL_ONE
+					}
+				>
+					<p style={{ textAlign: "center" }}>You start with Gold Crown</p>
+				</SettingSlider>
 			</div>
 
 			<Divider />
