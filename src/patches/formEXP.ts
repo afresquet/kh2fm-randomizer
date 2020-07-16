555d41357d017fa97a9129a451ccf3c2c80bac8f
afresquet <alvarofresquet@gmail.com>
2020-07-16 12:41:52 +0200

    refactored form EXP logic

--- src/patches/formEXP.ts
+++ src/patches/formEXP.ts
@@ -6,58 +6,19 @@ import { Multiplier } from "../settings/enums";
 export const formEXPPatch = (configuration: Configuration): string => {
 	const codes: string[] = [];
 
-	if (configuration.settings.valorEXP !== Multiplier.ONE) {
-		for (const level of formEXP.VALOR) {
-			const exp = Math.ceil(
-				level.required / (configuration.settings.valorEXP + 1)
-			);
-			const hex = exp.toString(16);
+	for (const [setting, levels] of [
+		[configuration.settings.valorEXP, formEXP.VALOR],
+		[configuration.settings.wisdomEXP, formEXP.WISDOM],
+		[configuration.settings.limitEXP, formEXP.LIMIT],
+		[configuration.settings.masterEXP, formEXP.MASTER],
+		[configuration.settings.finalEXP, formEXP.FINAL],
+	] as const) {
+		if (setting !== Multiplier.ONE) {
+			for (const level of levels) {
+				const exp = Math.ceil(level.required / (setting + 1));
 
-			codes.push(createLine(level.value, hex));
-		}
-	}
-
-	if (configuration.settings.wisdomEXP !== Multiplier.ONE) {
-		for (const level of formEXP.WISDOM) {
-			const exp = Math.ceil(
-				level.required / (configuration.settings.wisdomEXP + 1)
-			);
-			const hex = exp.toString(16);
-
-			codes.push(createLine(level.value, hex));
-		}
-	}
-
-	if (configuration.settings.limitEXP !== Multiplier.ONE) {
-		for (const level of formEXP.LIMIT) {
-			const exp = Math.ceil(
-				level.required / (configuration.settings.limitEXP + 1)
-			);
-			const hex = exp.toString(16);
-
-			codes.push(createLine(level.value, hex));
-		}
-	}
-
-	if (configuration.settings.masterEXP !== Multiplier.ONE) {
-		for (const level of formEXP.MASTER) {
-			const exp = Math.ceil(
-				level.required / (configuration.settings.masterEXP + 1)
-			);
-			const hex = exp.toString(16);
-
-			codes.push(createLine(level.value, hex));
-		}
-	}
-
-	if (configuration.settings.finalEXP !== Multiplier.ONE) {
-		for (const level of formEXP.FINAL) {
-			const exp = Math.ceil(
-				level.required / (configuration.settings.finalEXP + 1)
-			);
-			const hex = exp.toString(16);
-
-			codes.push(createLine(level.value, hex));
+				codes.push(createLine(level.value, exp.toString(16)));
+			}
 		}
 	}
 
