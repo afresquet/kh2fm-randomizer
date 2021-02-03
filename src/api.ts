eb589570096e2e684c0b359abc1acb97c749a8f7
afresquet <alvarofresquet@gmail.com>
2021-02-03 20:33:54 +0100

    deleted react app

--- src/api.ts
+++
@@ -1,4 +0,0 @@
-export const api: string =
-	process.env.NODE_ENV === "production"
-		? "https://us-central1-kh2fm-randomizer.cloudfunctions.net/randomizer"
-		: "http://localhost:5001/kh2fm-randomizer/us-central1/randomizer";
