8d832b7bca322c0935a58ab2c98b02fba7c9abc0
afresquet <29437693+afresquet@users.noreply.github.com>
2025-02-21 16:23:24 +0100

    nix flake

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  nixConfig = {
+    extra-substituters = "https://devenv.cachix.org";
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      devenv,
+      systems,
+      ...
+    }@inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+        devenv-test = self.devShells.${system}.default.config.test;
+      });
+
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+
+            modules = [
+              (
+                { pkgs, lib, ... }:
+                {
+                  languages.rust = {
+                    enable = true;
+                    # https://devenv.sh/reference/options/#languagesrustchannel
+                    channel = "stable";
+
+                    components = [
+                      "rustc"
+                      "cargo"
+                      "clippy"
+                      "rustfmt"
+                      "rust-analyzer"
+                    ];
+                  };
+
+                  packages = with pkgs; [
+                    bacon
+                  ];
+                }
+              )
+            ];
+          };
+        }
+      );
+    };
+}
